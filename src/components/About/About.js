29578062670500ba0db01fa327130f37c1146072
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-10-08 15:57:42 -0700

    I actually misspelt Cambridge.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,7 +11,7 @@ const About = () => (
         <Layout.Content>
             <Typography.Title level={3}>Hello!</Typography.Title>
             <Typography.Paragraph>
-                I'm Ruairi Dorrity, a full-stack software engineer based in San Francisco. I studied Computer Science at the University of Cambrdige before moving to the US. Right now, I do full-stack things at <ExternalLink href="https://engineeringblog.yelp.com/">Yelp</ExternalLink>. Outside of work, I spend most of my time tinkering with side projects, making music, and watching, playing or talking about <Tooltip title="⚽, not 🏈">football</Tooltip>.
+                I'm Ruairi Dorrity, a full-stack software engineer based in San Francisco. I studied Computer Science at the University of Cambridge before moving to the US. Right now, I do full-stack things at <ExternalLink href="https://engineeringblog.yelp.com/">Yelp</ExternalLink>. Outside of work, I spend most of my time tinkering with side projects, making music, and watching, playing or talking about <Tooltip title="⚽, not 🏈">football</Tooltip>.
             </Typography.Paragraph>
             <Typography.Paragraph>
                 Sometimes I do other stuff as well:
