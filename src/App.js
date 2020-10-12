ff0b4de9ec47ed26c033c83071f74e515ba5bb03
Ruairi Dorrity <ruairidorrity@gmail.com>
2020-10-12 15:41:28 -0700

    Added LinkedIn and Github redirects.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import HttpsRedirect from 'react-https-redirect';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 
 import './App.css';
 import MainPage from './pages/MainPage/MainPage';
 
 
+const LINKEDIN_URL = "https://www.linkedin.com/in/ruairi-dorrity-438919102/";
+const GITHUB_URL = "https://github.com/RuairiD";
+
+
 function App() {
     return (
         <HttpsRedirect>
-            <MainPage />
+            <BrowserRouter>
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Switch>
+                        <Route
+                            exact
+                            path="/"
+                            render={() => <MainPage />}
+                        />
+                        <Route
+                            exact
+                            path="/linkedin"
+                            component={() => { 
+                                window.location.href = LINKEDIN_URL; 
+                                return null;
+                            }}
+                        />
+                        <Route
+                            exact
+                            path="/github"
+                            component={() => { 
+                                window.location.href = GITHUB_URL; 
+                                return null;
+                            }}
+                        />
+                    </Switch>
+                </Suspense>
+            </BrowserRouter>
         </HttpsRedirect>
     );
 }
